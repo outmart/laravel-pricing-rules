2bfe088c3dc668cc1f9e495d1aac4afeeba02c89
Karim Mohamed <komicho1996@gmail.com>
2022-11-23 00:51:49 +0200

    Feat: create a `Lay` class to create rules

---
+++ src/Lay.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace OutMart\Laravel\PricingRules;
+
+class Lay
+{
+    private int $total;
+    private string $shippingMethod;
+    private string $paymentMethod;
+
+    /**
+     * Get the value of total
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * Set the value of total
+     *
+     * @return  self
+     */
+    public function setTotal($total)
+    {
+        $this->total = $total;
+    }
+
+    /**
+     * Get the value of shipping_method
+     */
+    public function getShippingMethod()
+    {
+        return $this->shippingMethod;
+    }
+
+    /**
+     * Set the value of shipping_method
+     *
+     * @return  self
+     */
+    public function setShippingMethod($shippingMethod)
+    {
+        $this->shippingMethod = $shippingMethod;
+    }
+
+    /**
+     * Get the value of paymentMethod
+     */
+    public function getPaymentMethod()
+    {
+        return $this->paymentMethod;
+    }
+
+    /**
+     * Set the value of paymentMethod
+     *
+     * @return  self
+     */
+    public function setPaymentMethod($paymentMethod)
+    {
+        $this->paymentMethod = $paymentMethod;
+    }
+
+    /**
+     * Set the value of paymentMethod
+     *
+     * @return  self
+     */
+    public function rule($case, $acion = null)
+    {
+        $attributes = [
+            'total' => $this->total,
+            'shipping_method' => $this->shippingMethod,
+            'payment_method' => $this->paymentMethod,
+        ];
+
+        $case = $case($attributes);
+
+        if ($case && $acion) {
+            $acion($this);
+        }
+    }
+}
